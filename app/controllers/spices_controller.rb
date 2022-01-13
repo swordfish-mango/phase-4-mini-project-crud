b4048e9e5cba21dfc2b15032ff1e9845f671849c
Marcus <marcus.shimotsu@gmail.com>
2022-01-13 12:50:12 -0600

    commit tree d7a054a394a4b52ed9633c13ec3630a2e1e194ea

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,26 @@
+class SpicesController < ApplicationController
+    def index
+        spices = Spice.all
+        render json: spices
+    end
+
+    def create
+        spice = Spice.create(strong_params)
+        render json: spice, status: :created
+    end
+
+    def update
+        spice = Spice.find_by(id: params[:id])
+        spice.update(strong_params)
+        render json: spice
+    end
+
+    def destroy
+        spice = Spice.find_by(id: params[:id])
+        spice.destroy
+        render json: {}
+    end
+    def strong_params
+        params.permit(:title,:image,:description,:notes,:rating)
+    end
+end
